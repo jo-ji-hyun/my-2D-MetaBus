c7f2caeb37404bfea8015f7e77bac10834e65041
jo-ji-hyun <newterr@naver.com>
2025-07-25 14:27:06 +0900

    [Feat]basecontroller 완성
    이동, 바라보는 방향 로직 기본 구성 완료

--- Assets/Scripts/Controller/BaseController.cs
+++ Assets/Scripts/Controller/BaseController.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class BaseController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    protected Rigidbody2D _rigidbody;                             // 물리 법칙 구현를 위해 
+    [SerializeField] private SpriteRenderer characterSprite;      // SpriteRenderer를 inspector창에서 조절할 수 있게 함
+
+    //=== 이동, 바라보는 방향 초기 값 ===
+    protected Vector2 _move = Vector2.zero;
+    protected Vector2 _look = Vector2.zero;
+
+    //=== 입력 받아옴 ===
+    public Vector2 Move { get { return _move; } }
+    public Vector2 look { get { return _look; } }
+
+    protected virtual void Awake()
     {
-        
+        _rigidbody = GetComponent<Rigidbody2D>(); // 컴퍼넌트에서 값을 가져옴
     }
 
-    // Update is called once per frame
-    void Update()
+    protected virtual void Start()
     {
-        
+
+    }
+
+    protected virtual void Update()
+    {
+        Rotate(_look);
+    }
+
+    protected virtual void FixedUpdate() // 움직임을 업데이트하기 위함
+    {
+        Movement(_move);
+    }
+
+    private void Movement(Vector2 direction)
+    {
+        direction = 5 * direction;
+
+        _rigidbody.velocity = direction; // rigidbody에서 속도 수치 조절
+    }
+
+    // 캐릭터가 보는 방향으로 캐릭터 뒤집기
+    private void Rotate(Vector2 direction) 
+    {
+        float rotZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        bool isLeft = Mathf.Abs(rotZ) > 90f;
+
+        characterSprite.flipX = isLeft;
     }
 }
--- Assets/Scripts/Controller/PlayerController.cs
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -3,36 +3,7 @@ using System.Collections.Generic;
 using Unity.IO.LowLevel.Unsafe;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : BaseController
 {
-    protected Rigidbody2D _rigidbody;                             // 물리 법칙 구현를 위해 
-    [SerializeField] private SpriteRenderer characterSprite;      // SpriteRenderer를 inspector창에서 조절할 수 있게 함
 
-    //=== 이동, 바라보는 방향 초기 값 ===
-    protected Vector2 _move = Vector2.zero;
-    protected Vector2 _look = Vector2.zero;
-
-    //=== 입력 받아옴 ===
-    public Vector2 Move { get { return _move; } }
-    public Vector2 look { get { return _look; } }
-
-    private  void Awake()
-    {
-        _rigidbody = GetComponent<Rigidbody2D>(); // 컴퍼넌트에서 값을 가져옴
-    }
-
-    protected virtual void Start()
-    {
-
-    }
-
-    protected virtual void Update()
-    {
-
-    }
-
-    private void FixedUpdate()
-    {
-
-    }
 }
