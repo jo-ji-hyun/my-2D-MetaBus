4cd4e12d03f97ab6c1fe092fbf54ea8838037c42
jo-ji-hyun <newterr@naver.com>
2025-07-25 19:09:10 +0900

    [Fix] 윗배경 추가 및 주석 달기
    +속도 점프 캡슐화

--- Assets/Scripts/MiniGame/Player/Plane.cs
+++ Assets/Scripts/MiniGame/Player/Plane.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class Plane : MonoBehaviour
 {
+    // === 일단 비워 ===
     Animator _animator = null;
     Rigidbody2D _rigidbody = null;
 
-    public float flapForce = 6.0f;
-    public float forwardSpeed = 3.0f;
+    // === 점프, 속도 수치 조절===
+    [SerializeField] private float flapForce = 6.0f;
+    [SerializeField] private float forwardSpeed = 3.0f;
 
     public bool isDead = false;
     bool _isFlap = false;
@@ -32,13 +34,14 @@ public class Plane : MonoBehaviour
 
     void Update()
     {
-        if (isDead)
+        if (isDead) //게임 오버
         {
-            //창을 띄워여함
+            //창을 띄워여함 메인씬으로 이동
         }
         else
         {
-            if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) // 스페이스 바, 마우스 왼쪽으로 점프 
+            // === 스페이스 바, 마우스 왼쪽으로 점프 호출 ===
+            if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) 
             {
                 _isFlap = true;
             }
@@ -54,14 +57,15 @@ public class Plane : MonoBehaviour
         Vector3 velocity = _rigidbody.velocity;
         velocity.x = forwardSpeed;
 
-        if (_isFlap)
+        if (_isFlap)                    //점프
         {
             velocity.y += flapForce;
             _isFlap = false;
         }
 
-        _rigidbody.velocity = velocity;
+        _rigidbody.velocity = velocity; //속도 조절
 
+        // === 포물선으로 떨어지도록 만듬 ===
         float angle = Mathf.Clamp((_rigidbody.velocity.y * 10f), -90, 90);
         transform.rotation = Quaternion.Euler(0, 0, angle);
     }
