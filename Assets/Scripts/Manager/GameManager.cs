c527be82eb663ed37b9116a09e9f4b61f60728c4
jo-ji-hyun <newterr@naver.com>
2025-07-27 02:28:37 +0900

    [Fix]미니게임 입장시 중력이 없음

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -13,15 +13,15 @@ public class GameManager : MonoBehaviour
     private UIManager uiManager;
     private UIHelper uiHelper;
 
-    private bool _miniGameZone; // 미니게임 입장시 true
-    private int _currentScore;  // 현재 점수
-    private int _FinalScore;    // 최종 점수
+    private bool _miniGameStart;    // 미니게임 입장시 true
+    private bool _mainGameStart;     // 메인씬(메타버스)에 있을 경우
+    private int _currentScore = 0;    // 현재 점수
+    private int _FinalScore = 0;       // 최종 점수
 
     public static GameManager Instance { get; private set; } // 싱글톤 선언
 
     private void Awake()
     {
-
         uiManager = FindObjectOfType<UIManager>();
         uiHelper = FindObjectOfType <UIHelper>();
 
@@ -36,71 +36,84 @@ public class GameManager : MonoBehaviour
 
         SceneManager.sceneLoaded += OnSceneLoaded; // 씬 로드
 
-        Time.timeScale = 0.0f; // 중력 때문에 게임을 일시정지 시킴
+        Time.timeScale = 0.0f; // 처음 시작시
     }
 
     private void Start()
     {
-        uiHelper.Setactive(1); // 미니게임 점수판 끄기
+
     }
 
     private void Update()
     {
-        if (Input.anyKeyDown && _miniGameZone == true) // 미니게임에서 아무키나 누르면 시작함
+        if (Input.anyKeyDown && _miniGameStart == true) // 미니게임에서 아무키나 누르면 시작함
         {
             GameStart();
         }
+
+        else if (Input.anyKeyDown && _mainGameStart == true)
+        {
+            uiHelper.Setactive(1);                         // 미니게임 점수판 끄기
+        }
+
     }
     private void OnDestroy()
     {
-        // GameManager 오브젝트가 파괴될 때 이벤트 구독 해제 (클린업)
+        // GameManager 오브젝트가 파괴될 때 비우기
         SceneManager.sceneLoaded -= OnSceneLoaded;
     }
 
     // === 현재 씬 로드 ===
     private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
-
+        // === 미니게임의 경우 ===
         if (scene.name == miniGameSceneName)
         {
+            Time.timeScale = 0.0f; // 중력 때문에 게임을 일시정지 시킴
+
             uiManager = FindObjectOfType<UIManager>();
-            _miniGameZone = true;
+            _miniGameStart = true;
+            _mainGameStart = false;
             _currentScore = 0; // 점수 초기화
         }
+        // === 메인씬(메타버스)의 경우 ===
         else if (scene.name == mainSceneName)
         {
             uiHelper = FindObjectOfType<UIHelper>();
             Time.timeScale = 1.0f; // 움직임을 위해
-            _miniGameZone = false;
+            _mainGameStart = true;
+
             _FinalScore = PlayerPrefs.GetInt("FinalScore", 0);  // 0점 부터 불러옴
 
-            if (_FinalScore != 0)
+            if (_FinalScore != 0) // 점수가 있을 경우
             {
-                Debug.Log("최고점수 저장");
                 uiHelper.Setactive(0);                           // 미니게임 점수판 켜기
                 uiHelper.ViewScoreBoard(_FinalScore);
             }
         }
     }
 
+    // === 미니게임 시작 ===
     public void GameStart()
     {
         Plane._isMoving = true;
-        _miniGameZone = false; // Update() 그만 호출하기 위해
+        _miniGameStart = false; // Update() 그만 호출하기 위해
         Time.timeScale = 1.0f;
 
         uiManager.Setactive(1); // 설명창 끄기
     }
 
+    // === 미니게임 오버 ===
     public void GameOver()
     {
         // === 현재점수를 FinalScore로 저장합니다. ===
         PlayerPrefs.SetInt("FinalScore", _currentScore);
         PlayerPrefs.Save();
 
-        SceneManager.LoadScene(mainSceneName);
+        SceneManager.LoadScene(mainSceneName); // 메인(메타버스)으로 이동
     }
 
+    // === 점수 획득 ===
     public void AddScore(int score)
     {
         _currentScore += score;
