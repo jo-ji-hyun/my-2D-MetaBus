f04fa14a0e79737d19f27eb6cc1c30b77acd500c
jo-ji-hyun <newterr@naver.com>
2025-07-27 13:27:32 +0900

    [Feat]메인씬 카메라 이동 완료

--- Assets/Scripts/Metabus_Map/Controller/CameraController.cs
+++ Assets/Scripts/Metabus_Map/Controller/CameraController.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // === inspector창에 타겟을 받음 ===
+    public Transform target;
+    float _offsetX;
+    float _offsetY;
+
     void Start()
     {
-        
+        if (target == null)
+            return;
+
+        // === 타깃과의 x, y 거리 ===
+        _offsetX = transform.position.x - target.position.x;
+        _offsetY = transform.position.y - target.position.y;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (target == null)
+            return;
+
+        Vector3 pos = transform.position;
+
+        // === 기존의 카메라 위치에 x 거리를 더함 ===
+        pos.x = target.position.x + _offsetX;
+        pos.y = target.position.y + _offsetY;
+
+        transform.position = pos; // 새 위치로 이동
     }
 }
