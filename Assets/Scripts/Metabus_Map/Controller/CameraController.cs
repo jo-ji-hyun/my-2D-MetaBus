f3555edffe182ee3e4489156d60ea69814b28b1a
jo-ji-hyun <newterr@naver.com>
2025-07-27 13:36:54 +0900

    [Feat]카메라 범위 고정

--- Assets/Scripts/Metabus_Map/Controller/CameraController.cs
+++ Assets/Scripts/Metabus_Map/Controller/CameraController.cs
@@ -9,6 +9,13 @@ public class CameraController : MonoBehaviour
     float _offsetX;
     float _offsetY;
 
+    // === 카메라 이동 제한 ===
+    [SerializeField] private float minX = 0.7384f; // 최소 X 좌표
+    [SerializeField] private float maxX = 2.2984f;  // 최대 X 좌표
+
+    [SerializeField] private float minY = 0.1798f;   // 최소 Y 좌표
+    [SerializeField] private float maxY = 4.0798f;    // 최대 Y 좌표
+
     void Start()
     {
         if (target == null)
@@ -26,10 +33,15 @@ public class CameraController : MonoBehaviour
 
         Vector3 pos = transform.position;
 
-        // === 기존의 카메라 위치에 x 거리를 더함 ===
+        // === 1. 기존의 카메라 위치에 x 거리를 더함 ===
         pos.x = target.position.x + _offsetX;
         pos.y = target.position.y + _offsetY;
 
-        transform.position = pos; // 새 위치로 이동
+        // ===  2.최솟값 최대값 고정 ===
+        pos.x = Mathf.Clamp(pos.x, minX, maxX);
+        pos.y = Mathf.Clamp(pos.y, minY, maxY);
+
+        // === 3. 새 위치 반환 ===
+        transform.position = pos;
     }
 }
