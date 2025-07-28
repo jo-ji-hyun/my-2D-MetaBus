377739f3722b939798a62857a499f130d18e7745
jo-ji-hyun <newterr@naver.com>
2025-07-28 09:37:10 +0900

    [Docs] 옷장만듬

--- Assets/Scripts/Metabus_Map/UI/UIClothes.cs
+++ Assets/Scripts/Metabus_Map/UI/UIClothes.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class UIClothes : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject title;
+
+    public void OpenMenu()
     {
-        
+        title.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ClosedMenu()
     {
-        
+        title.SetActive(false);
     }
 }
